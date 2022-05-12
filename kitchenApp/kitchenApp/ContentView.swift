ed61d248cd35be0985ecb23c462c8d7a8e558b97
Willi <80400277+Willi-Pindjukov@users.noreply.github.com>
2022-05-12 09:33:37 +0200

    added projectsetup

---
+++ kitchenApp/kitchenApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kitchenApp
+//
+//  Created by Willi Pindjukov on 06.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kitchenApp/kitchenApp/kitchenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  kitchenAppApp.swift
+//  kitchenApp
+//
+//  Created by Willi Pindjukov on 06.05.22.
+//
+
+import SwiftUI
+
+@main
+struct kitchenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
