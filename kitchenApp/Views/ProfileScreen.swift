bcd25dfe5028ba0986a6477982200ecd3bc6a7d5
Olga Molenova <olgamolenova@gmail.com>
2022-06-15 00:13:49 +0200

    Initial Version StartScreen ProfileScreen TransactionsScreen

---
+++ kitchenApp/Views/ProfileScreen.swift
@@ -0,0 +1,129 @@
+//
+//  ProfileScreen.swift
+//  kitchenApp
+//
+//  Created by Olga Molenova on 14.06.22.
+//
+
+import SwiftUI
+import MapKit
+
+struct Profile {
+    var name: String
+    var password: String
+    var customID: String
+    var balance: String
+}
+
+struct ProfileScreen: View {
+    
+    @State var profile: Profile
+    @State var mode: EditMode = .inactive
+    @State var isCancelled = false
+    
+    @State var edit = false
+    @State var showAlert = false
+    
+    var body: some View {
+        
+        Group {
+            if mode.isEditing {
+                editProfileView()
+            } else {
+                ProfileView()
+            }
+        }
+    }
+    
+    
+    fileprivate func ProfileView() -> some View {
+        return  VStack {
+            Form{
+                Section(header: Text("My Info")) {
+                    HStack {
+                        Text("Name")
+                        Spacer()
+                        Text(profile.name).foregroundColor(.secondary)
+                    }
+                    
+                    HStack {
+                        Text("Password")
+                        Spacer()
+                        Text(profile.password).foregroundColor(.secondary)
+                    }
+                    
+                    HStack {
+                        Text("Custom ID")
+                        Spacer()
+                        Text(profile.customID).foregroundColor(.secondary)
+                    }
+                    
+                    HStack {
+                        Text("Balance")
+                        Spacer()
+                        Text(profile.balance).foregroundColor(.secondary)
+                    }
+                    
+                    HStack {
+                        Text("Purchases")
+                        Spacer()
+                            .foregroundColor(.secondary)
+                        NavigationLink(destination: TransactionsScreen()) {}
+                    }
+                }
+                
+            }
+            
+            Button("Sign off?") {
+                showAlert = true
+            }
+            .alert(isPresented: $showAlert) {
+                Alert (
+                    title: Text("You are signed off"),
+                    message: Text("")
+                )
+            }
+        }
+        
+        .navigationBarTitle(Text("Profile"))
+        .navigationBarItems(trailing: EditButton())
+        .navigationBarBackButtonHidden(true)
+        .environment(\.editMode, self.$mode)
+    }
+    
+    
+    fileprivate func editProfileView() -> some View {
+        return Form {
+            
+            Section(header:Text("Name")) {
+                TextField("Name", text: $profile.name)
+            }
+            
+            Section(header:Text("Password")) {
+                TextField("Password", text: $profile.password)
+            }
+            Section(header:Text("Custom ID")) {
+                TextField("Custom ID", text: $profile.customID)
+                    .disabled(edit == false)
+                    .foregroundColor(.secondary)
+            }
+            Section(header:Text("Balance")) {
+                TextField("Balance", text: $profile.balance)
+                    .disabled(edit == false)
+                    .foregroundColor(.secondary)
+            }
+        }
+        
+        //"Cancel" still is not working very well
+        .navigationBarTitle(Text("Edit Profile"))
+        .navigationBarItems(leading: Button(action: {}) {NavigationLink(destination: ProfileView()) {Text("Cancel")}}, trailing: EditButton())
+        .environment(\.editMode, self.$mode)
+    }
+    
+}
+
+struct ProfileScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileScreen(profile: Profile(name: "Peter", password: "peterchen13", customID: "12345", balance: "20$"))
+    }
+}
---
+++ kitchenApp/Views/StartScreen.swift
@@ -0,0 +1,79 @@
+//
+//  StartScreen.swift
+//  kitchenApp
+//
+//  Created by Olga Molenova on 14.06.22.
+//
+
+import SwiftUI
+
+enum AnimationState {
+    case normal
+    case compress
+    case expand
+}
+
+struct StartScreen: View {
+    
+    @State private var animationState: AnimationState = .normal
+    @State private var done: Bool = false
+    
+    
+    func calculate() -> Double {
+        
+        switch animationState {
+        case .normal:
+            return 0.2
+        case .compress:
+            return 0.18
+        case .expand:
+            return 10.0
+        }
+    }
+    
+    
+    var body: some View {
+        
+        NavigationView {
+            ZStack {
+                
+                //Here instead of ProfileScreen() call LoginScreen(),
+                //still test data.
+                //link ProfilScreen() in LoginScreen with test data
+                ProfileScreen(profile: Profile(name: "Peter", password: "peterchen13", customID: "12345", balance: "20$"))
+                VStack {
+                    Image("Image-2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .scaleEffect(calculate())
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.green)
+                    .opacity(done ? 0: 1)
+                
+            }
+            .navigationBarHidden(done ? false: true)
+            
+            .onAppear(){
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    withAnimation(.spring()) {
+                        animationState = .compress
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            withAnimation(.spring()) {
+                                animationState = .expand
+                                withAnimation(.interpolatingSpring(mass: 1.0, stiffness: 100.0, damping: 10.0, initialVelocity: 0)) {
+                                    done = true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct StartScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        StartScreen()
+    }
+}
