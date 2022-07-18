4f6bdf945d052f6cbdb6b4ed3b1c23a4a7542900
Willi <80400277+Willi-Pindjukov@users.noreply.github.com>
2022-07-18 14:28:16 +0200

    Feat: added Mainscreen for user and admin
    Added two MainScreens. One for User and one for Admin

---
+++ kitchenApp/kitchenApp/Views/MainScreen.swift
@@ -0,0 +1,70 @@
+//
+//  MainScreen.swift
+//  kitchenApp
+//
+//  Created by Willi Pindjukov on 17.07.22.
+//
+
+import SwiftUI
+import MapKit
+
+
+struct MainScreen: View {
+    @ObservedObject var profile = ProfileViewModel()
+    
+    @StateObject private var loginVM = LoginViewModel()
+    
+    @State var editProfile = Profile(name: "",password: "")
+    
+    var body: some View {
+        VStack {
+            CoffeeImage()
+            HStack {
+                Text(self.profile.userProfile.name).frame(height: 30)
+            }
+            HStack {
+                Text("Balance:")
+                Text(self.profile.userProfile.balance, format: .number).frame(height: 100)
+            }
+            List{
+                NavigationLink(destination: BookingView()) {
+                Button(action: {
+                    loginVM.login()
+                }) {
+                    BookItemButtonContent()
+                }
+            }
+            NavigationLink(destination: ProfileScreen()) {
+                Button(action: {
+                    loginVM.login()
+                }) {
+                    EditProfileButtonContent()
+                }
+            }
+            }
+        }
+        .padding()
+        .onAppear(perform: {
+            profile.getUserData()
+        })
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen()
+    }
+}
+
+struct CoffeeImage : View {
+    var body: some View {
+        return Image("Image-2")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
+    }
+}
+
