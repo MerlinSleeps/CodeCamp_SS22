b8df3e7bd21f32932c700f8f08798da20bbe54fe
Merlin Möller <merlin199919@gmail.com>
2022-07-18 11:59:24 +0200

    Added GetUsers and small cleanup
    Added GetUsers Request and cleaned up booking and orderView

---
+++ kitchenApp/kitchenApp/Views/ShowAllUserScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ShowAllUserScreen.swift
+//  kitchenApp
+//
+//  Created by Merlin Möller on 18.07.22.
+//
+
+import SwiftUI
+
+struct ShowAllUserScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShowAllUserScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowAllUserScreen()
+    }
+}
