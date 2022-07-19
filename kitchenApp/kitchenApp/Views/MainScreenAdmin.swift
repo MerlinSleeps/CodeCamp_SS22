30d3c8f58e6175c8d84a3a4362a3dcc4d6cb862b
Willi <80400277+Willi-Pindjukov@users.noreply.github.com>
2022-07-19 15:30:43 +0200

    fix: coffeeImageView in mainscreenadmin
    coffeeImageView in mainscreenadmin

--- kitchenApp/kitchenApp/Views/MainScreenAdmin.swift
+++ kitchenApp/kitchenApp/Views/MainScreenAdmin.swift
@@ -18,7 +18,7 @@ struct MainScreenAdmin: View {
     
     var body: some View {
         VStack {
-            CoffeeImage()
+            CoffeeImageView()
             HStack {
                 Text(self.profile.userProfile.name).frame(height: 30)
             }
