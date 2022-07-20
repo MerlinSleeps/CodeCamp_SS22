a47360207ee56b033428c66df1179d2c495e7dbd
Willi <80400277+Willi-Pindjukov@users.noreply.github.com>
2022-07-20 16:23:09 +0200

    Feat: UserImageView
    implemented UserImageView for Show all User Task

---
+++ kitchenApp/kitchenApp/Views/Utillities/UserImageView.swift
@@ -0,0 +1,27 @@
+//
+//  UserImageView.swift
+//  kitchenApp
+//
+//  Created by Willi Pindjukov on 20.07.22.
+//
+
+import SwiftUI
+
+struct UserImageView: View {
+    @ObservedObject var profile = ProfileViewModel()
+    
+    var body: some View {
+        return Image("UserImages")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 70, height: 70)
+            .clipped()
+            .cornerRadius(150)
+    }
+}
+
+struct UserImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserImageView()
+    }
+}
