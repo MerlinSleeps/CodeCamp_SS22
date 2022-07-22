f82622dc42ee4dfe35a79f0e517b92c58e85af38
Merlin Möller <merlin199919@gmail.com>
2022-07-22 15:56:52 +0200

    added image

--- kitchenApp/kitchenApp/Views/Utillities/UserImageView.swift
+++ kitchenApp/kitchenApp/Views/Utillities/UserImageView.swift
@@ -11,7 +11,7 @@ struct UserImageView: View {
     @ObservedObject var profile = ProfileViewModel()
     
     var body: some View {
-        return Image("UserImages")
+        return Image("Image")
             .resizable()
             .aspectRatio(contentMode: .fill)
             .frame(width: 70, height: 70)
