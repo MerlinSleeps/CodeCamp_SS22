d3ec34fb5d0113c3040b9f28a59682fdaa4cc204
Merlin Möller <merlin199919@gmail.com>
2022-07-22 14:06:55 +0200

    login data for convienice

--- kitchenApp/kitchenApp/ViewModels/LoginViewModel.swift
+++ kitchenApp/kitchenApp/ViewModels/LoginViewModel.swift
@@ -10,11 +10,8 @@ import JWTDecode
 
 class LoginViewModel: ObservableObject {
     
- //   @Published var id: String = "a3620095-0598-415f-89d6-f382a6e9d9c8"
-  //  @Published var password: String = "iOSGroupA"
-        
-    @Published var id: String = ""
-    @Published var password: String = ""
+    @Published var id: String = "a3620095-0598-415f-89d6-f382a6e9d9c8"
+    @Published var password: String = "iOSGroupA"
     @Published var isAuthenticated: Bool = false    
     @Published var isAdmin: Bool = false
     @Published var message: String = ""
