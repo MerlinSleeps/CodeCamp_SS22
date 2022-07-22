193c0cae4a431f84b4cde74f7c03b4acda857a53
Merlin Möller <merlin199919@gmail.com>
2022-07-22 14:45:15 +0200

    deleted junk var from user

--- kitchenApp/kitchenApp/Models/User.swift
+++ kitchenApp/kitchenApp/Models/User.swift
@@ -11,12 +11,4 @@ import SwiftUI
 struct User: Hashable, Codable, Identifiable {
     let id: String
     let name: String
-    
-    private var imageName: String
-    
-    var image: Image {
-
-        Image(imageName)
-
-    }
 }
