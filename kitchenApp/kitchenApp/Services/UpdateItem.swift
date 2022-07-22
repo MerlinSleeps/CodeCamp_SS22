48dbbf926599063409d925b6068726150926c64e
szzfish <1808495779@qq.com>
2022-07-22 16:42:21 +0200

    implemented sort for transaction histories

--- kitchenApp/kitchenApp/Services/UpdateItem.swift
+++ kitchenApp/kitchenApp/Services/UpdateItem.swift
@@ -136,8 +136,73 @@ func getMostpopularItems() -> [String] {
     var othersAmountCount = 0
     itemSummeList.sort(by: >)
     for (key, value) in itemPurchaseSumme {
-        if (value == itemSummeList[0] || value == itemSummeList[1] || value == itemSummeList[2] || value == itemSummeList[3] || value == itemSummeList[4] || value == itemSummeList[5] || value == itemSummeList[6] || value == itemSummeList[7] || value == itemSummeList[8]) {
+        if (value == itemSummeList[0] ) {
             itemNames.append(key)
+        } else if (value == itemSummeList[1] || value == itemSummeList[2] || value == itemSummeList[3] || value == itemSummeList[4] || value == itemSummeList[5] || value == itemSummeList[6] || value == itemSummeList[7] || value == itemSummeList[8]) {
+        } else {
+            othersSummeCount += value
+        }
+    }
+    for (key, value) in itemPurchaseSumme {
+        if (value == itemSummeList[1] ) {
+            itemNames.append(key)
+        } else if (value == itemSummeList[0] || value == itemSummeList[2] || value == itemSummeList[3] || value == itemSummeList[4] || value == itemSummeList[5] || value == itemSummeList[6] || value == itemSummeList[7] || value == itemSummeList[8]) {
+        } else {
+            othersSummeCount += value
+        }
+    }
+    for (key, value) in itemPurchaseSumme {
+        if (value == itemSummeList[2]) {
+            itemNames.append(key)
+        } else if (value == itemSummeList[0] || value == itemSummeList[1] || value == itemSummeList[3] || value == itemSummeList[4] || value == itemSummeList[5] || value == itemSummeList[6] || value == itemSummeList[7] || value == itemSummeList[8]) {
+        } else {
+            othersSummeCount += value
+        }
+    }
+    for (key, value) in itemPurchaseSumme {
+        if (value == itemSummeList[3]) {
+            itemNames.append(key)
+        } else if (value == itemSummeList[0] || value == itemSummeList[1] || value == itemSummeList[2] || value == itemSummeList[4] || value == itemSummeList[5] || value == itemSummeList[6] || value == itemSummeList[7] || value == itemSummeList[8]) {
+        } else {
+            othersSummeCount += value
+        }
+    }
+    for (key, value) in itemPurchaseSumme {
+        if (value == itemSummeList[4]) {
+            itemNames.append(key)
+        } else if (value == itemSummeList[0] || value == itemSummeList[1] || value == itemSummeList[2] || value == itemSummeList[3] || value == itemSummeList[5] || value == itemSummeList[6] || value == itemSummeList[7] || value == itemSummeList[8]) {
+        } else {
+            othersSummeCount += value
+        }
+    }
+    for (key, value) in itemPurchaseSumme {
+        if (value == itemSummeList[5]) {
+            itemNames.append(key)
+        } else if (value == itemSummeList[0] || value == itemSummeList[1] || value == itemSummeList[2] || value == itemSummeList[3] || value == itemSummeList[4] || value == itemSummeList[6] || value == itemSummeList[7] || value == itemSummeList[8]) {
+        } else {
+            othersSummeCount += value
+        }
+    }
+    for (key, value) in itemPurchaseSumme {
+        if (value == itemSummeList[6]) {
+            itemNames.append(key)
+        } else if (value == itemSummeList[0] || value == itemSummeList[1] || value == itemSummeList[2] || value == itemSummeList[3] || value == itemSummeList[4] || value == itemSummeList[5] || value == itemSummeList[7] || value == itemSummeList[8]) {
+        } else {
+            othersSummeCount += value
+        }
+    }
+    for (key, value) in itemPurchaseSumme {
+        if (value == itemSummeList[7]) {
+            itemNames.append(key)
+        } else if (value == itemSummeList[0] || value == itemSummeList[1] || value == itemSummeList[2] || value == itemSummeList[3] || value == itemSummeList[4] || value == itemSummeList[5] || value == itemSummeList[6] || value == itemSummeList[8]) {
+        } else {
+            othersSummeCount += value
+        }
+    }
+    for (key, value) in itemPurchaseSumme {
+        if (value == itemSummeList[8]) {
+            itemNames.append(key)
+        } else if (value == itemSummeList[0] || value == itemSummeList[1] || value == itemSummeList[2] || value == itemSummeList[3] || value == itemSummeList[4] || value == itemSummeList[5] || value == itemSummeList[6] || value == itemSummeList[7]) {
         } else {
             othersSummeCount += value
         }
